fc8149a4f77815901fd90bef0ff7949b96440684
Zac Ong <zac.ong@bartleboglehegarty.com>
2016-06-13 10:18:16 +0800

    html update

--- js/components/src/Home.js
+++ js/components/src/Home.js
@@ -7,44 +7,38 @@ import { Link } from 'react-router'
 class Home extends React.Component {
 	render() {
 		return (
-			<section className='landingpage'>
-				<div id='slideshow'>
-				   <div>
-					   <img src="/images/landingpage/hero01.jpg" />
-				   </div>
-				   <div>
-					   <img src="/images/landingpage/hero02.jpg" />
-				   </div>
-				   <div>
-					   <img src="/images/landingpage/hero03.jpg" />
-				   </div>
-				</div>
-
-				<div className='logo'>
-					<Link to='/'><img src='/images/logo_light.png' className='img-responsive' /></Link>
-				</div>
-
-				<div className='nav-arrow'>
-					<Link to='/map'><img src='/images/landingpage/arrow.png' /></Link>
-				</div>
-			</section>
+			<section id="landingpage">
+		    <div class="container-fluid">
+		    <div class="row">
+		        <div class=".col-xs-12 .col-sm-12 col-md-12 nopadding">
+		          <div class="logo">
+		            <img src="images/logo_light.png"/>
+		          </div>
+		          <div class="fadein">
+		            <img src="images/hero01.jpg"/>
+		            <img src="images/hero02.jpg"/>
+		            <img src="images/hero03.jpg"/>
+		            <img src="images/hero04.jpg"/>
+		            <img src="images/hero05.jpg"/>
+		            <img src="images/hero06.jpg"/>
+		            <img src="images/hero07.jpg"/>
+		            <img src="images/hero08.jpg"/>
+		            <img src="images/hero09.jpg"/>
+		            <img src="images/hero10.jpg"/>
+		          </div>
+		          <a href="map.html">
+		            <div class="arrow bounce">
+		            </div>
+		          </a>
+		        </div>
+		    </div>
+		    </div>
+		  </section>
 		)
 	}
-	componentDidMount() {
-		$("#slideshow > div:gt(0)").hide();
-
-		this.intervalID = setInterval(function() {
-			$('#slideshow > div:first')
-				.fadeOut(1000)
-				.next()
-				.fadeIn(1000)
-				.end()
-				.appendTo('#slideshow');
-		}, 5000);
-	}
-	componentWillUnmount() {
-		clearInterval(this.intervalID);
-	}
-}
+	$(function(){
+		 $('.fadein img:gt(0)').hide();
+			setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+		});
 
 module.exports = Home;
